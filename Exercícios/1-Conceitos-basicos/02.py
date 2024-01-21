e68125c6a446c949c482c17c8b0b30056d491004
luanaxcardoso <luanacardoso.bc@gmail.com>
2024-01-21 15:51:14 -0300

    pasta

--- Exercícios/1-Conceitos-basicos/02.py
+++
@@ -1,9 +0,0 @@
-# 2- Peça ao usuário para informar o ano de nascimento. Em seguida, calcule e imprima a idade atual.
-
-ano_nascimento = int(input("Digite o ano de nascimento: "))
-
-ano_atual = int(input("Digite o ano atual: "))
-
-idade = ano_atual - ano_nascimento
-
-print(f"\nVocê tem {idade} anos de idade.")
--- Exercícios/3-Listas-Tuplas/05.py
+++
@@ -1,8 +0,0 @@
-# 5. Crie duas tuplas. Concatene-as para formar uma nova tupla. 
-
-tupla1 = (1, 2, 3,4,5,6,7,8,9,10)
-tupla2 = ('a', 'b', 'c', 'd', 'e')
-
-nova_tupla = tupla1 + tupla2
-
-print(nova_tupla)
