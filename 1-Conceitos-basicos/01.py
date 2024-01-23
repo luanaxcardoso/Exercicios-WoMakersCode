6a75d54355f37f63be8c0de0e16eccead8eee18b
Luana Cardoso <luanacardoso.bc@gmail.com>
2024-01-23 14:21:45 -0300

    Update 01.py

--- 1-Conceitos-basicos/01.py
+++ 1-Conceitos-basicos/01.py
@@ -1,8 +1,15 @@
 # 1- Faça um Programa que peça dois números e realize as principais operações: soma, subtração, multiplicação, divisão em python
 
-#f é para formatar a string e \n é para pular linha
+#Para mudar a cor das frases no terminal usei o Colorama
+#Para instalar o Colorama
+#pip install colorama
+#pip install colorama --user
 
-num1 = int(input("Digite o primeiro número: "))
+import colorama
+
+colorama.init()
+
+num1 = int(input( colorama.Fore.CYAN + "Digite o primeiro número: "))
 num2 = int(input("Digite o segundo número: "))
 
 soma = num1 + num2
@@ -11,8 +18,8 @@ multiplicacao = num1 * num2
 divisao = num1 / num2
 divisao_inteira = num1 // num2
 
-print(f"\nNúmeros digitados são: {num1} e {num2}")
-print(f"\nResultados:")
+print(colorama.Fore.LIGHTYELLOW_EX + f"\nNúmeros digitados são: {num1} e {num2}")
+print(colorama.Fore.MAGENTA + f"\nResultados:")
 print(f"Soma: {soma}")
 print(f"Subtração: {subtracao}")
 print(f"Multiplicação: {multiplicacao}")
